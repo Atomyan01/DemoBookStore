8f504c1bd24bb986dfdb5c51c0be488b69237c5c
Atomyan01 <atomyanashot5@gmail.com>
2025-03-10 15:53:16 +0400

    Nayelu

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DemoBookStore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
